5605f85704e93ea38c59b35c832ce0b968bdc1f5
RomanaKarasenko <143837231+RomanaKarasenko@users.noreply.github.com>
2024-05-25 00:06:58 +0300

    Steps-1-3

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+    try {
+        // Читаємо існуючі контакти з файлу
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        const contacts = JSON.parse(data);
+
+        // Генеруємо новий контакт
+        const newContact = createFakeContact();
+
+        // Додаємо новий контакт до існуючого масиву
+        contacts.push(newContact);
+
+        // Записуємо оновлений масив назад у файл
+        await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+
+        console.log('Successfully added one new contact.');
+      } catch (error) {
+        console.error('Error adding new contact:', error);
+      }
+};
 
 await addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,30 @@
-import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-const generateContacts = async (number) => {};
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  try {
+    // Читаємо існуючі контакти з файлу
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    // Генеруємо нові контакти
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+
+    // Додаємо нові контакти до існуючого масиву
+    const updatedContacts = [...contacts, ...newContacts];
+
+    // Записуємо оновлений масив назад у файл
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+
+    console.log(`Successfully added ${number} new contacts.`);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 
 await generateContacts(5);
